0e7b34021b2e42de86f51e40993722ce80848462
arun <arunnn023@gmail.com>
2025-07-18 16:40:40 +0530

    Add GitHub Actions workflow

---
+++ git-action/src/test/java/com/neoteric/git_action/GitActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoteric.git_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
